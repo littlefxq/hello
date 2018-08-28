bdbe745fe824b43355783dea4e8fc6bb8ab8e7ea
xqin <1135793503@qq.com>
2018-08-28 11:02:30 +0800

    hello1

--- conf/default.py
+++ conf/default.py
@@ -30,10 +30,10 @@ from django.conf.global_settings import *  # noqa
 # 应用基本信息配置 (请按照说明修改)
 # ==============================================================================
 # 在蓝鲸智云开发者中心 -> 点击应用ID -> 基本信息 中获取 APP_ID 和 APP_TOKEN 的值
-APP_ID = ''
-APP_TOKEN = ''
+APP_ID = 'hello'
+APP_TOKEN = 'f00259ae-7c3a-4372-9997-95baa2f32226'
 # 蓝鲸智云开发者中心的域名，形如：http://paas.bking.com
-BK_PAAS_HOST = ''
+BK_PAAS_HOST = 'http://paas.bk.com'
 
 # 是否启用celery任务
 IS_USE_CELERY = True
